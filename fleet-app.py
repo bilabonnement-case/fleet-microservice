0ccf3bdadb516539288175ee706cb92de979d3e2
BirkLauritzen <birklauritzen@gmail.com>
2024-12-17 01:54:45 +0100

    Update fleet-app.py

--- fleet-app.py
+++ fleet-app.py
@@ -4,9 +4,13 @@ import sqlite3
 from datetime import datetime
 from dotenv import load_dotenv
 from flasgger import Swagger, swag_from
+from flask_cors import CORS
 
 # Load environment variables
 load_dotenv()
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "https://api-gateway.azurewebsites.net"}})
 
 app = Flask(__name__)
 
